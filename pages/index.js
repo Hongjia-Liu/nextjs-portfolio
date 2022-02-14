c13d783b0e6758cbfd2a086ff2a82b5d0df25fd5
Hongjia Liu <hongjia_liu@outlook.com>
2022-02-14 00:56:53 +0000

    update Home & Projects page

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,53 @@
+import {
+	Heading,
+	Flex,
+	VStack,
+	HStack,
+	Icon,
+	Text,
+	Link,
+	Avatar,
+} from "@chakra-ui/react";
+
+import { FaTwitter, FaGithub, FaLinkedin } from "react-icons/fa";
+
 export default function Home() {
-	return <h1>Home Page</h1>;
+	return (
+		<VStack align="center" gap="32px" justify="center">
+			<Avatar src="/anime-dom.png" size="2xl" />
+			<VStack>
+				<Heading as="h1">Hongjia Liu</Heading>
+				<Heading size="md" as="h3">
+					On my way to be a Developer without Limitations
+				</Heading>
+			</VStack>
+			<Text textAlign="center">
+				👋 Hi, 你好! I will tell you what I will do and what I will not
+				do. I will not serve that in which I no longer believe, whether
+				it call itself my home, my fatherland, or my church: and I will
+				try to express myself in some mode of life or art as freely as I
+				can and as wholly as I can, using for my defence the only arms I
+				allow myself to use — silence, exile, and cunning.
+				<br /> Lets connect 👇
+			</Text>
+			<HStack spacing="20px" justifySelf="center">
+				<Link
+					href="https://www.twitter.com/Hongjia__Liu"
+					target="_blank"
+				>
+					<Icon color="blue.400" as={FaTwitter} />
+				</Link>
+				<Link href="https://www.github.com/Hongjia-Liu" target="_blank">
+					<Icon color="black" as={FaGithub} />
+				</Link>
+
+				<Link
+					href="https://www.linkedin.com/in/hongjia-liu"
+					target="_blank"
+				>
+					<Icon color="blue.600" as={FaLinkedin} />
+				</Link>
+			</HStack>
+		</VStack>
+	);
 }
--- pages/projects.js
+++ pages/projects.js
@@ -1,5 +1,15 @@
-import { Heading } from "@chakra-ui/react";
+import { VStack } from "@chakra-ui/react";
+import ProjectCard from "@components/ProjectCard";
 
 export default function Projects() {
-	return <Heading as="h1">Projects Page</Heading>;
+	return (
+		<VStack spacing={10} mt={10} justify="flex-start" direction="column">
+			<ProjectCard
+				image="/portfolio.png"
+				altMessage="A screenshot of my portfolio project"
+				name="Portfolio"
+				link="https://github.com/Hongjia-Liu/nextjs-portfolio"
+			/>
+		</VStack>
+	);
 }
